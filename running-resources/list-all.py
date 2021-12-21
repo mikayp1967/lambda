7960d0f5e57319f1df2b62a9a0948df3eba7da69
Michele P <mikayp1967@gmail.com>
2021-12-21 12:06:56 +0000

    Add eventbridge event, code mods for diff actions + debug

--- running-resources/list-all.py
+++ running-resources/list-all.py
@@ -9,14 +9,14 @@ import os
 # Have switch function to allow stopping or even deleting stray resources
 # I test this on a box with python 3.7.10 so I guess run with that version
 
-sns = boto3.client('sns')
-sns_arn=os.environ['MY_SNS_TOPIC_ARN']
-instance_list=""
+print("Defining stuff...")
+
 
 
 
 def check_EC2():
 # List of EC2s
+    instance_list=""
     EC2=boto3.resource('ec2', region_name="eu-west-2")
     all_instances=EC2.instances.all()
 
@@ -24,8 +24,11 @@ def check_EC2():
         print(f'EC2 instance {instance.id}  \t State: {instance.state["Name"]}')
     #    print(f'Tags: {instance.tags.value}')
         instance_list=instance_list + '\n ' +instance.id + "\t" + instance.state["Name"]
+    return instance_list
 
 def send_SNS(subject,message):
+    sns = boto3.client('sns')
+    sns_arn=os.environ['MY_SNS_TOPIC_ARN']
     response = sns.publish(
         TopicArn=sns_arn,
         Message=message,
@@ -34,12 +37,13 @@ def send_SNS(subject,message):
 
 
 def lambda_handler(event, context):
+    EC2_list=""
     action=event['Action']
     if (action =="List"):
-        check_EC2()
-        send_SNS("Running Instances", instance_list)
+        EC2_list=check_EC2()
+        send_SNS("Running Instances", EC2_list)
     elif (action == "Debug"):
-        ret_str=(json.dumps(event) + "\nsns_arn: " + sns_arn)
+        ret_str=(json.dumps(event) + "\nMY_SNS_TOPIC_ARN: " + os.environ['MY_SNS_TOPIC_ARN'])
         send_SNS("Debug info", ret_str)
     else:
         send_SNS("ERROR", json.dumps(event))
@@ -47,7 +51,6 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps(instance_list)
+        'body': json.dumps(EC2_list)
 
     }
-
